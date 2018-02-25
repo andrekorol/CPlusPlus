1751d82896ad3af96c23f804b702eb38d431131d
andrekorol <anrobits@yahoo.com.br>
2018-02-25 18:19:55 -0300

    Add constexpr tests

---
+++ sandbox/cpp/cppreference-constexpr.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdexcept>
+
+constexpr int factorial(int n)
+{
+	return n <= 1 ? 1 : (n * factorial(n - 1));
+}
+
+class conststr {
+	const char* p;
+	std::size_t sz;
+public:
+	template<std::size_t N>
+	constexpr conststr(const char(&a)[N]): p(a), sz(N - 1) {}
+
+	constexpr char operator[](std::size_t n) const
+	{
+		return n < sz ? p[n] : throw std::out_of_range("");
+	}
+	constexpr std::size_t size() const { return sz; }
+};
+
+constexpr std::size_t countlower(conststr s, std::size_t n = 0,
+								 std::size_t c = 0)
+{
+	return n == s.size() ? c :
+		'a' <= s[n] && s[n] <= 'z' ? countlower(s, n + 1, c + 1) :
+		countlower(s, n + 1, c);
+}
+
+template<int n>
+struct constN
+{
+	constN() { std::cout << n << '\n'; }
+};
+
+int main()
+{
+	std::cout << "4! = ";
+	constN<factorial(4)> out1;
+
+	volatile int k = 8;
+	std::cout << k << "! = " <<factorial(k) << '\n';
+
+	std::cout << "the number of lowercase letters in \"Hello, world!\" is ";
+	constN<countlower("Hello, world!")> out2;
+}
